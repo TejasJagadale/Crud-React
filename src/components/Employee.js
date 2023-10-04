4465eb0b5e215879970bd6f2d648315cef6018ab
TejasJagadale <tejasjagadale43@gmail.com>
2023-10-04 18:20:34 +0530

    CrudOperation-React

---
+++ src/components/Employee.js
@@ -0,0 +1,34 @@
+const Employee = [
+    // {
+    //     id: "1",
+    //     Name: "Tejas",
+    //     Age : "23",
+    // },
+    // {
+    //     id: "2",
+    //     Name: "Ram",
+    //     Age : "23",
+    // },
+    // {
+    //     id: "3",
+    //     Name: "Surya",
+    //     Age : "25",
+    // },
+    // {
+    //     id: "4",
+    //     Name: "Viswes",
+    //     Age : "22",
+    // },
+    // {
+    //     id: "5",
+    //     Name: "Sarves",
+    //     Age : "21",
+    // },
+    // {
+    //     id: "5",
+    //     Name: "Prem",
+    //     Age : "21",
+    // },
+]
+
+export default Employee;
